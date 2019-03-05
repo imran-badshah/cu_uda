0de09ab6492437e44673bf614caf243573c9fbf1
imran <imrnbdshiscool@yahoo.co.in>
2019-03-05 21:14:37 +0100

    48. greyscale intro

--- main.cu
+++ main.cu
@@ -1,10 +1,47 @@
 #include <stdio.h>
 
 // __global__ : declaration specifier (aka declspec): a C-language construct => CUDA knows this is a kernel and not CPU code
-__global__ void cube(float *d_out, float *d_in) {
-    int idx = threadIdx.x; //(dim3: a C-Struct with .x .y and .z)tells each thread its index within a block
-    float f = d_in[idx];
-    d_out[idx] = f * f *f;
+__global__ void rgba_to_greyscale(const uchar4* const rbgaImage, unsigned char* const greyImage, int numRows, int numCols) {
+    // TO-DO:
+    // Fill in the kernel to convert from colour to greyscale
+    // the mapping from components of a uchar4 to RGBA is:
+    // .x -> R; .y -> G; .z -> B; .w -> A
+    //
+    // The output (greyImage) at each pixel should be the result of
+    // applying the formula: output = .299f * R + .587f *G + .114f * B;
+    // Note: We will be ignoring the alpha channel for this conversion
+
+    // First create a mapping from the 2D block and grid locations
+    // to an absolute 2D location in the image, then use that to
+    // calculate a 1D offset
+    int y = threadIdx.y + blockIdx.y * blockDim.y;
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    if (y < numCols && x < numRows) {
+        int index = numRows * y + x;
+        uchar4 colour = rbgaImage[index];
+        unsigned char grey = (unsigned char)(0.299f * colour.x + 0.587f * colour.y + 0.114f * colour.z);
+        greyImage[index] = grey;
+    }
+}
+
+void your_rgba_to_greyscale(const uchar4* const h_rgbaImage, uchar4* const d_rgbaImage, 
+                            unsigned char* const d_greyImage, size_t numRows, size_t numCols)
+{
+    // You must fill in the correct sizes for the blockSize and gridSize
+    // currently only one block with one thread is being launched
+    // const dim3 blockSize(1, 1, 1); // TO-DO
+    int blockWidth = 32;
+    const dim3 blockSize(blockWidth, blockWidth, 1);
+
+    // const dim3 gridSize(numCols, numRows, 1); // TO-DO
+    int blocksX = numRows / blockWidth + 1;
+    int blocksY = numCols / blockWidth + 1;
+    const dim3 gridSize(blocksX, blocksY, 1);
+
+    rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
+
+    cudaDeviceSynchronize();
+    checkCudaErrors(cudaGetLastError());
 }
 
 int main(int argc, char **argv) {
